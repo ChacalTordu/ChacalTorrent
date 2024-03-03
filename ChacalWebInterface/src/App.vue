c2bd86a3abf82977e62a72b101f0a6daaccea3e5
chacaltordu <leo.lebrun04@gmail.com>
2024-03-03 15:57:17 +0100

    commit dirty

--- ChacalWebInterface/src/App.vue
+++ ChacalWebInterface/src/App.vue
@@ -9,18 +9,15 @@ Author: ChacalTordu
 <template>
   <div class="myApp">
     <div class="title"><p>Chacal Torrent</p></div>
-      <div class="cards">
-        <div class="cardsRow" v-for="(row, index) in componentRows" :key="index">
-          <div v-for="(item, rowIndex) in componentList" :key="rowIndex" class="cardItem">
+      <div class="cards" v-for="(item, rowIndex) in componentList" :key="rowIndex">
             <cardTorrent @downloadClicked="handleDownloadClicked"/>
-          </div>
-        </div>
+
       </div>
   </div>
 </template>
 
 <script setup>
-import { ref, computed } from "vue";
+import { ref } from "vue";
 import cardTorrent from "./components/cardTorrent.vue";
 
 const componentList = ref([cardTorrent]);
@@ -29,20 +26,6 @@ function handleDownloadClicked() {
   console.log("callback handleDownloadClicked appelé")
   componentList.value.push(cardTorrent)
 }
-
-const componentRows = computed(() => {
-  const rows = [];
-  const components = componentList.value;
-  const numComponent = components.length;
-  
-  let rowIndex = 0;
-
-  for (let i = 0; i < numComponent; i+=3) {
-    rows.push(components.slice(i,i+3));
-  }
-  return rows;
-
-});
 
 </script>
 
@@ -67,13 +50,8 @@ const componentRows = computed(() => {
 .cards {
   display: flex;
   justify-content: center;
+  flex-direction: row;
   flex-wrap: wrap;
-}
-
-.cardsRow {
-  display: flex;
-  flex-wrap: wrap;
-  /* gap: 80px; */
-  margin-bottom: 20px;
+  gap: 80px;
 }
 </style>
