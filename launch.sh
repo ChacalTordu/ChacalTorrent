f2a451b59974bad888b9ee201be6fc4cdfd34640
Auguste <tanguy.lepennec@gmail.com>
2024-03-03 08:35:29 +0100

    :wrench: -  Launcher script

---
+++ launch.sh
@@ -0,0 +1,6 @@
+echo "Running FileManager..."
+python3 ChacalFileManagement/main.py &
+echo "Launching Front interface..."
+npm run preview --prefix ChacalWebInterface -- --host --port 1998 &
+echo "Starting Backend server..."
+node ./ChacalWebInterface/serverBack/server.js
