eff2f62e42b57881b37a5d664c209ead4e4eedcb
Leo LE-BRUN <leo.le-brun@apside.com>
2024-03-19 15:21:16 +0100

    [FIX] : launching loop

--- launch.sh
+++ launch.sh
@@ -1,6 +1,8 @@
 echo "Starting Backend server..."
 node ./serverNode/src/index.js &
+
 echo "Running FileManager..."
 python3 ChacalFileManagement/main.py &
+
 echo "Launching Front interface..."
-npm run preview --prefix ChacalWebInterface -- --host --port 1998 &
+npm run preview --prefix ChacalWebInterface -- --host --port 1998
