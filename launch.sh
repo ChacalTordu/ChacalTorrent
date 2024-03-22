cb648428c6384fa9c780a0aee1c7555b27c4074d
Leo LE-BRUN <leo.le-brun@apside.com>
2024-03-22 10:17:26 +0100

    [FIX] : Launch order

--- launch.sh
+++ launch.sh
@@ -1,8 +1,8 @@
-echo "Running Backend server..."
-node ./serverNode/src/index.js &&
-
 echo "Running FileManager..."
 python3 ChacalFileManagement/main.py &
 
 echo "Launching Front interface..."
-npm run preview --prefix ChacalWebInterface -- --host --port 1998
+npm run preview --prefix ChacalWebInterface -- --host --port 1998 &
+
+echo "Running Backend server..."
+node ./serverNode/src/index.js 
